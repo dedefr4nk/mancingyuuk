408996a8684d1c02dd8858392715bf67d6e67a1f
m4verick <frederik.sabandar@gmail.com>
2015-10-06 14:12:12 +0700

    * some files updated

--- src/config/define.js
+++ src/config/define.js
@@ -1,2 +1,4 @@
 // define.js :
 // define all macro needed or prepro needed in game
+
+const DEV_MODE = 'false' //use for debugging only
--- src/config/utility.js
+++ src/config/utility.js
@@ -3,11 +3,15 @@
 
 function Utility()
 {
-    this.log = function(str) {
-      //get from http://stackoverflow.com/questions/20524700/custom-console-log-function-a-console-log-wrapper
-      var line = trace();
-      var lines = line.split("\n");
-      console.log(x + " " + lines[2].substring(lines[2].indexOf("("), lines[2].lastIndexOf(")") + 1));
+    this.log = function(str) 
+	{
+		if(DEV_MODE)
+		{
+			//get from http://stackoverflow.com/questions/20524700/custom-console-log-function-a-console-log-wrapper
+			var line = trace();
+			var lines = line.split("\n");
+			console.log(x + " " + lines[2].substring(lines[2].indexOf("("), lines[2].lastIndexOf(")") + 1));
+		}
     };
 }
 var Utility = new Utility();
